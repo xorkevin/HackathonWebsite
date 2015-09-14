1ab056e89abb604565739bc787e013c4c52dbc6d
Kevin Wang <wangkevin448@gmail.com>
2015-09-14 00:58:38 -0700

    now the routing FINALLY works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 import angular from 'angular';
+import router from 'angular-new-router';
+import {app} from '../module.js';
+// import {HomeController} from '../components/home/home';
+// import {NavbarController} from '../components/navbar/navbar';
+import '../components/home/home';
+import '../components/navbar/navbar';
 
-angular.module('app' ,[]);
+class AppController {
+  constructor($router){
+    $router.config([
+      {path: '/home', component: 'home'},
+      {path: '/', redirectTo: '/home'}
+    ]);
+    this.world = 'Hello, world!';
+  }
+}
+
+app.controller('AppController', ['$router', AppController]);
+  // .controller('HomeController', [HomeController])
+  // .controller('NavbarController', [NavbarController]);
